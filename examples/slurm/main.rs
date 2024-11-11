6920a36f6d4199b0c7964bfa34f2ffe3639efaae
Gregory Eslinger <gregjesl@gmail.com>
2024-11-11 12:12:44 -0500

    Iss/2 (#3)
    * Added retry loop
    
    * Removed sleep
    
    * Fixed retry method
    
    * Slurm example uses retries and timeout

--- examples/slurm/main.rs
+++ examples/slurm/main.rs
@@ -6,6 +6,8 @@ fn main() {
     let mut host = "localhost".to_string();
     let mut port = 7878;
     let mut id = std::process::id().to_string();
+    let mut timeout = Duration::new(1, 0);
+    let mut retries: u64 = 0;
 
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
@@ -34,11 +36,36 @@ fn main() {
                 id = args.first().unwrap().to_string();
                 args.remove(0);
             }
+            "--timeout" => {
+                args.remove(0);
+                let timeout_str = args.first().unwrap().to_string();
+                for c in timeout_str.chars() {
+                    if !c.is_numeric() { 
+                        panic!("Invalid retry count");
+                    }
+                }
+                let timeout_sec = timeout_str.parse::<u64>().expect("Could not parse retry count");
+                timeout = Duration::new(timeout_sec, 0);
+                args.remove(0);
+            }
+            "--retries" => {
+                args.remove(0);
+                let retries_str = args.first().unwrap().to_string();
+                for c in retries_str.chars() {
+                    if !c.is_numeric() { 
+                        panic!("Invalid retry count");
+                    }
+                }
+                retries = retries_str.parse::<u64>().expect("Could not parse retry count");
+                args.remove(0);
+            }
             &_ => { panic!("Unexpected arguement"); }
         }
     }
 
     let mut client = Client::new(host, port);
+    client.with_timeout(timeout)
+        .with_retries(retries);
 
     loop {
         match client.query() {
